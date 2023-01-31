6d131be28e83b685a1c788608515a885035e82a9
adalysg <adalys39@gmail.com>
2023-01-31 18:43:27 -0500

    Working on message tests

--- car_wash_processor.py
+++ car_wash_processor.py
@@ -7,3 +7,18 @@ class CarWashDecisionProcessor:
     def get_weather_data(self):
         return self.noaa_interface.get_weather(self.lat, self.long)
 
+    def do_wash(self, weather_data):
+        # if statements?
+        # Need to differentiate between rain or snow and return corresponding bool
+        # (EX: if raining, but not snowing in state like FL, what do I return?)
+        if weather_data['snow'] >= 10 and weather_data['rain'] >= 10:
+            return True
+        return False
+
+    def wash_car_message(self):
+        yes_wash_car_message = 'Today is a good day to wash your car.'
+        no_do_not_wash_car_message = 'You should not was your car today.'
+        if self.do_wash == True:
+            print(no_do_not_wash_car_message)
+        else:
+            print(yes_wash_car_message)
--- test_carwashdecisionprocessor.py
+++ test_carwashdecisionprocessor.py
@@ -14,9 +14,23 @@ class TestCarWashDecisionProcessor(TestCase):
         weather_data = car_wash_decision_processor.get_weather_data()
         noaa_interface.get_weather.assert_called_once_with("fake_lat", "fake_long")
         # Now that we have weather data, what do we do with it?
-    
-    def weather_message(self, weather_data):
-        # Precipitation ID = "PRCP"
-        check_rain_or_snow = CheckRainOrSnow()
-        check_rain_or_snow.yes_no_rain()
-        check_rain_or_snow.yes_no_snow()
+
+    # Through the next two tests, we now know that do_wash should take in weather_data in the 
+    # format of a dict with certain keys and return a boolean depending on the keys' values
+    def test_should_say_yes_to_wash_car(self):
+        car_wash_decision_processor = CarWashDecisionProcessor("fake_lat", "fake_long", Mock())
+        weather_data = {"rain": 14, "snow": 10}
+        res = car_wash_decision_processor.do_wash(weather_data)
+        self.assertTrue(res)
+
+    def test_should_say_no_to_wash_car(self):
+        car_wash_decision_processor = CarWashDecisionProcessor("fake_lat", "fake_long", Mock())
+        weather_data = {"rain": 4, "snow": 1}
+        res = car_wash_decision_processor.do_wash(weather_data)
+        self.assertFalse(res)
+
+    # Depending on boolean, we print a certain type of message
+    def test_should_print_correct_no_message_depending_on_res_of_weather_data(self):
+        # I want to get the above function (test_should_say_yes), and test the wash_car_message 
+        # function in the car_wash_processor.py file
+        self.assertEqual('You should not was your car today.')
