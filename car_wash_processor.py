1628e9a608a84668ab7e917a8001159c72afc401
adalysg <adalys39@gmail.com>
2023-02-02 13:52:32 -0500

    Added the wash_car_message function to class and 'should_print_correct_yes/no_message' tests

--- car_wash_processor.py
+++ car_wash_processor.py
@@ -8,17 +8,19 @@ class CarWashDecisionProcessor:
         return self.noaa_interface.get_weather(self.lat, self.long)
 
     def do_wash(self, weather_data):
-        # if statements?
-        # Need to differentiate between rain or snow and return corresponding bool
-        # (EX: if raining, but not snowing in state like FL, what do I return?)
-        if weather_data['snow'] >= 10 and weather_data['rain'] >= 10:
+        # Will return TRUE if it's a good day to wash car (values <10)
+        # Will return FALSE if it's not a good day to wash car (values >=10)
+        if weather_data['snow'] < 10 and weather_data['rain'] < 10:
             return True
         return False
 
-    def wash_car_message(self):
+    def wash_car_message(self, do_wash_bool):
         yes_wash_car_message = 'Today is a good day to wash your car.'
         no_do_not_wash_car_message = 'You should not was your car today.'
-        if self.do_wash == True:
-            print(no_do_not_wash_car_message)
+        print(do_wash_bool)
+        # do_wash returns True = you SHOULD wash car --> YES message 
+        if do_wash_bool == True:
+            return yes_wash_car_message
+        # do_wash returns False = you should NOT wash car --> NO message
         else:
-            print(yes_wash_car_message)
+            return no_do_not_wash_car_message
--- test_carwashdecisionprocessor.py
+++ test_carwashdecisionprocessor.py
@@ -17,20 +17,29 @@ class TestCarWashDecisionProcessor(TestCase):
 
     # Through the next two tests, we now know that do_wash should take in weather_data in the 
     # format of a dict with certain keys and return a boolean depending on the keys' values
-    def test_should_say_yes_to_wash_car(self):
+    def test_should_say_no_to_wash_car(self):
         car_wash_decision_processor = CarWashDecisionProcessor("fake_lat", "fake_long", Mock())
         weather_data = {"rain": 14, "snow": 10}
         res = car_wash_decision_processor.do_wash(weather_data)
-        self.assertTrue(res)
+        self.assertFalse(res)
 
-    def test_should_say_no_to_wash_car(self):
+    def test_should_say_yes_to_wash_car(self):
         car_wash_decision_processor = CarWashDecisionProcessor("fake_lat", "fake_long", Mock())
         weather_data = {"rain": 4, "snow": 1}
         res = car_wash_decision_processor.do_wash(weather_data)
-        self.assertFalse(res)
+        self.assertTrue(res)
 
     # Depending on boolean, we print a certain type of message
     def test_should_print_correct_no_message_depending_on_res_of_weather_data(self):
-        # I want to get the above function (test_should_say_yes), and test the wash_car_message 
-        # function in the car_wash_processor.py file
-        self.assertEqual('You should not was your car today.')
+        car_wash_decision_processor = CarWashDecisionProcessor("fake_lat", "fake_long", Mock())
+        weather_data = {"rain": 12, "snow": 10}
+        res_no = car_wash_decision_processor.do_wash(weather_data)
+        no_message = car_wash_decision_processor.wash_car_message(res_no)
+        self.assertEqual(no_message, 'You should not was your car today.')
+
+    def test_should_print_correct_yes_message_depending_on_res_of_weather_data(self):
+        car_wash_decision_processor = CarWashDecisionProcessor("fake_lat", "fake_long", Mock())
+        weather_data = {"rain": 4, "snow": 1}
+        res_yes = car_wash_decision_processor.do_wash(weather_data)
+        yes_message = car_wash_decision_processor.wash_car_message(res_yes)
+        self.assertEqual(yes_message, 'Today is a good day to wash your car.')
